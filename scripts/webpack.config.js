7affb6e4439de62dedc0b9e36b9223f22deb62c1
olton <serhii@pimenov.com.ua>
2023-11-14 17:28:37 +0200

    added import metro

--- scripts/webpack.config.js
+++ scripts/webpack.config.js
@@ -1,50 +1,67 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-  entry: './scripts/src/index.ts',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
+module.exports = [
+    {
+      entry: './scripts/src/index.ts',
+      module: {
+        rules: [
+          {
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/,
+          },
+          {
+            test: /\.less$/i,
+            use: [
+              // compiles Less to CSS
+              "style-loader",
+              "css-loader",
+              "less-loader",
+            ],
+          },
+        ],
       },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  output: {
-    library: {
-      name: 'GAZDA_CLIENT',
-      type: 'var'
-    },
-    filename: 'app-client.min.js',
-    path: path.resolve(__dirname, '../source/assets/js'),
-  },
-  plugins: [
-    new CleanWebpackPlugin({
-	  protectWebpackAssets: false,
-	  cleanOnceBeforeBuildPatterns: [],
-      cleanAfterEveryBuildPatterns: ['*.LICENSE.txt'],
-    })
-  ],
-  devServer: {
-    historyApiFallback: {
-      rewrites: [
-        { from: /^\/$/, to: '/index.html' },
-        { from: /^\/metroui/, to: '/metroui.html' },
-        { from: /^\/localizer/, to: '/localizer.html' },
-        { from: /^\/easyquery/, to: '/easyquery.html' },
-        { from: /^\/easy-report-starter-kit/, to: '/easy-report-starter-kit.html' },
-        { from: /./, to: '/404.html' },
+      resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+      },
+      output: {
+        library: {
+          name: 'GAZDA_CLIENT',
+          type: 'var'
+        },
+        filename: 'app-client.min.js',
+        path: path.resolve(__dirname, '../source/assets/js'),
+      },
+      plugins: [
+        new CleanWebpackPlugin({
+          protectWebpackAssets: false,
+          cleanOnceBeforeBuildPatterns: [],
+          cleanAfterEveryBuildPatterns: ['*.LICENSE.txt'],
+        })
       ],
     },
-    static: {
-      directory: path.join(path.resolve(__dirname, '../_site')),
-    },
-    compress: false,
-    port: 9000,
-  },
-};
+    {
+      entry: './scripts/src/metro.js',
+      module: {
+        rules: [
+          {
+            test: /\.less$/i,
+            use: [
+              // compiles Less to CSS
+              "style-loader",
+              "css-loader",
+              "less-loader",
+            ],
+          },
+        ],
+      },
+      resolve: {
+        extensions: ['.js'],
+      },
+      output: {
+        filename: 'metro.min.js',
+        path: path.resolve(__dirname, '../source/assets/js'),
+      },
+    }
+];
